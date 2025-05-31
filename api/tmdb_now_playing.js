2f5c73746246e5a84d94ceaafbdb620f9c9a97c0
Pavishni <pavishni.r@gmail.com>
2025-05-31 21:42:13 +0530

    added CORS origin and changed file name

--- api/tmdb_now_playing.js
+++ api/tmdb_now_playing.js
@@ -1,7 +1,16 @@
 export default async function handler(req, res) {
   const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
+   const allowedOrigins = [
+    "http://localhost:5173",
+    "https://netflixgpt-movies.web.app"
+  ];
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
